3f4927895c5ec50a69aa1db0d5a5bdfb125343c2
15541139895 <Liu2914666350@163.com>
2020-08-11 21:24:13 +0800

    修改MainActivity代码，实现按钮功能

--- app/src/main/java/com/example/gitmergeapplication/MainActivity.java
+++ app/src/main/java/com/example/gitmergeapplication/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.gitmergeapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+private int number =0;
+private TextView textView;
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.tvNumber);
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number++;
+                textView.setText(String.valueOf(number));
+            }
+        });
     }
 }
